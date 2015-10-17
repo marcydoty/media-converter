c5badb635861c345529e84827250bc5ead43133b
marcilene <mrasistemas@gmail.com>
2015-10-17 13:36:33 -0300

    Adicionando serviço encoder, realizando alguns testes com zencoder-java

--- src/main/java/com/marcilene/controller/UploadController.java
+++ src/main/java/com/marcilene/controller/UploadController.java
@@ -6,9 +6,7 @@ import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-
 import org.primefaces.model.UploadedFile;
-
 import com.marcilene.service.AmazonService;
 
 @Model
@@ -16,30 +14,33 @@ public class UploadController {
 
 	@Inject
 	private AmazonService amazonService;
-	
-    private UploadedFile file;
-    
-    private String fileUrl;
-    
-    public UploadedFile getFile() {
-        return file;
-    }
- 
-    public void setFile(UploadedFile file) {
-        this.file = file;
-    }
-     
-    public void upload() {
-        if(file != null) {
-        	try {
-        		fileUrl = amazonService.uploadFile(file.getInputstream(), file.getSize(), file.getFileName());
+	private UploadedFile file;
+
+	private String fileUrl;
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+
+	public void upload() {
+		if (file != null) {
+			try {
+
+				fileUrl = amazonService.uploadFile(file.getInputstream(),
+						file.getSize(), file.getFileName());
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
-            FacesContext.getCurrentInstance().addMessage(null, message);
-        }
-    }
+			FacesMessage message = new FacesMessage("Succesful",
+					file.getFileName() + " is uploaded.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
 
 	public String getFileUrl() {
 		return fileUrl;
@@ -48,4 +49,5 @@ public class UploadController {
 	public void setFileUrl(String fileUrl) {
 		this.fileUrl = fileUrl;
 	}
+
 }
--- src/main/java/com/marcilene/service/AmazonService.java
+++ src/main/java/com/marcilene/service/AmazonService.java
@@ -19,20 +19,28 @@ public class AmazonService {
 	private static final String SECRET_KEY = "secret_key";
 	private static final String URL_PATTERN = "https://s3-sa-east-1.amazonaws.com/{0}/{1}";
 
+
 	public String buildUrl(String fileKey) {
 		return MessageFormat.format(URL_PATTERN, BUCKET_NAME, fileKey);
 	}
-
-	public String uploadFile(InputStream input, Long size, String fileName) {
-		AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+	
+	
+	public String uploadFile(InputStream input, Long size, String fileName){
+		AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY,
+				SECRET_KEY);
 
 		String fileKey = UUID.randomUUID().toString() + fileName;
 		AmazonS3 s3client = new AmazonS3Client(credentials);
 		ObjectMetadata metadata = new ObjectMetadata();
+	
 
 		metadata.setContentLength(size);
-		s3client.putObject(new PutObjectRequest(BUCKET_NAME, fileKey, input, metadata)
-				.withCannedAcl(CannedAccessControlList.PublicRead));
+		s3client.putObject(new PutObjectRequest(BUCKET_NAME, fileKey, input,
+				metadata).withCannedAcl(CannedAccessControlList.PublicRead));
+
+		// String urlEncoder = encoderService.createJob(buildUrl(fileKey));
+
 		return buildUrl(fileKey);
 	}
+
 }
